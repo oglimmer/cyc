17ab9f8ae5dc6cbbb993c65997fb4356666f6a62
Oliver Zimpasser <oliver.zimpasser@adtech.com>
2013-11-28 18:51:24 +0100

    Updated management script

--- cyc_mgmt.sh
+++ cyc_mgmt.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-usage="$(basename "$0") [-b] [-w v] [-e n] - builds or deploys cyc
+usage="$(basename "$0") [-b|-a] [-w] [-e] - builds or deploys cyc
 
 where:
-    -h  show this help text
-    -b  build, performans a git pull and mvn package
-    -w  war, deploy the war file with the given version n
-    -e  engine, deploy the engine file with the given version n"
+    -h  shows this help text
+    -b  build: performans a git pull and mvn package
+    -a  auto build: builds/deploys only if there is a new commit in git
+    -w  war: deploys the war file with a new version
+    -e  engine: deploys the engine file with a new version"
 
-BUILD=
-WAR=
-ENGINE=
+if [ -z "$CYC_WEBAPPS" ]; then
+  CYC_WEBAPPS=/var/lib/tomcat/webapps
+fi
+if [ -z "$CYC_ENGINE_CONTAINER" ]; then
+  ENGINE_CONTAINER=/usr/local/cyr-engine-container
+fi
 
-while getopts ':hbwe' option; do
+while getopts ':hbwea' option; do
   case "$option" in
     h) echo "$usage"
        exit
@@ -22,6 +26,8 @@ while getopts ':hbwe' option; do
     w) WAR=YES
        ;;
     e) ENGINE=YES
+       ;;
+    a) AUTO_BUILD=YES
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -35,6 +41,16 @@ while getopts ':hbwe' option; do
 done
 shift $((OPTIND - 1))
 
+if [ -n "$AUTO_BUILD" ]; then
+  BUILD=
+  git=$(git pull)
+  if [ "$git" != "Already up-to-date." ]; then
+    mvn clean package 
+  else
+    WAR=
+    ENGINE=
+  fi
+fi
 
 if [ -n "$BUILD" ]; then
   git pull
@@ -42,23 +58,29 @@ if [ -n "$BUILD" ]; then
 fi
 
 if [ -n "$WAR" ]; then
-  for file in `ls -1 /var/lib/tomcat/webapps/cyr*.war | sort -r| awk '{gsub(/\/.*\/|cyr##|.war/,"",$1); printf("%03d\n",++$1);}'`
+  for file in `ls -1 $CYC_WEBAPPS/cyr* | sort -r| awk '{gsub(/\/.*\/|cyr##|.war/,"",$1); printf("%03d\n",++$1);}'`
   do
     VERSION=$file
     break
   done
+  if [ -z "$VERSION" ]; then
+    VERSION=001
+  fi
   echo "Deploying war with version = $VERSION"
-  cp web/target/cyr##001.war /var/lib/tomcat/webapps/cyr##$VERSION.war
+  cp web/target/cyr##001.war $CYC_WEBAPPS/cyr##$VERSION.war
 fi
 
 if [ -n "$ENGINE" ]; then
-        for file in `ls -1 /usr/local/cyr-engine-container/cyc* | sort -r| awk '{gsub(/\/.*\/|cyc##/,"",$1); printf("%03d\n",++$1);}'`
-        do
-                VERSION=$file
-                break
-        done
+  for file in `ls -1 $CYC_ENGINE_CONTAINER/cyc* | sort -r| awk '{gsub(/\/.*\/|cyc##/,"",$1); printf("%03d\n",++$1);}'`
+  do
+    VERSION=$file
+    break
+  done
+  if [ -z "$VERSION" ]; then
+    VERSION=001
+  fi
   echo "Deploying engine with version = $VERSION"
-  mkdir /usr/local/cyr-engine-container/tmp-deploy
-  cp engine/target/engine-0.1-SNAPSHOT-jar-with-dependencies.jar /usr/local/cyr-engine-container/tmp-deploy
-  mv /usr/local/cyr-engine-container/tmp-deploy /usr/local/cyr-engine-container/cyc$VERSION
+  mkdir $CYC_ENGINE_CONTAINER/tmp-deploy
+  cp engine/target/engine-jar-with-dependencies.jar $CYC_ENGINE_CONTAINER/tmp-deploy
+  mv $CYC_ENGINE_CONTAINER/tmp-deploy $CYC_ENGINE_CONTAINER/cyc$VERSION
 fi
