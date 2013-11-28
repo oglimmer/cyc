f38102f7b54b482482fc99ca1d36a7feaad3950a
Oliver Zimpasser <oliver.zimpasser@adtech.com>
2013-11-28 19:01:10 +0100

    Updated jar name in buildDist.sh

--- engine-container/src/main/scripts/buildDist.sh
+++ engine-container/src/main/scripts/buildDist.sh
@@ -4,7 +4,7 @@ mkdir -p target/dist
 cp target/engine-container-0.1-SNAPSHOT-jar-with-dependencies.jar target/dist/
 cp target/classes/security.policy target/dist/
 mkdir -p target/dist/cyc0
-cp ../engine/target/engine-0.1-SNAPSHOT-jar-with-dependencies.jar target/dist/cyc0
+cp ../engine/target/engine-jar-with-dependencies.jar target/dist/cyc0
 mkdir -p target/dist/logs
 echo "java -Djava.security.policy=security.policy -Dcyc.home=.  -jar engine-container-0.1-SNAPSHOT-jar-with-dependencies.jar" >target/dist/run.sh
 chmod 777 target/dist/run.sh
