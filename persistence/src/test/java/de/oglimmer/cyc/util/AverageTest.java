d30f2781c45e9976e946c486a320347e16f01408
Oli Zimpasser <oglimmer@gmail.com>
2013-12-27 17:53:58 +0100

    Added a unit test for Average

--- persistence/src/test/java/de/oglimmer/cyc/util/AverageTest.java
+++ persistence/src/test/java/de/oglimmer/cyc/util/AverageTest.java
@@ -1,5 +1,17 @@
 package de.oglimmer.cyc.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AverageTest {
+
+	@Test
+	public void testAverage() {
+		Average a = new Average();
+		a.add(5);
+		a.add(7);
+		a.add(9);
+		Assert.assertEquals(7, a.average(), 0d);
+	}
 
 }
