f9cb31332997fa42d4a13fc2260c397a617462dc
Oli Zimpasser <oglimmer@gmail.com>
2014-01-05 11:46:12 +0100

    Refactoring

--- rules/src/main/resources/de.oglimmer.cyc.api.MenuEntryRules.groovy
+++ rules/src/main/resources/de.oglimmer.cyc.api.MenuEntryRules.groovy
@@ -4,7 +4,11 @@ public class MenuEntryRuleImpl implements de.oglimmer.cyc.api.IMenuEntryRule {
 	
 	public org.slf4j.Logger log;
 	
-	public int getDeliciousness(Object ingredients, Object price) {		
+	public int getDeliciousness(Object ingredients) {		
+		return getDeliciousness(ingredients, 0);
+	}
+
+	public int getDeliciousness(Object ingredients, int type) {		
 		return ingredients.size();
 	}
 	
