7ffeba3437a386b8864a2c5851316ccb0f3fb180
Oli Zimpasser <oglimmer@gmail.com>
2014-03-08 12:31:02 +0100

    Added java SecurityManager support (after adding netty)

--- engine-container/src/main/java/de/oglimmer/cyc/engineContainer/GameServer.java
+++ engine-container/src/main/java/de/oglimmer/cyc/engineContainer/GameServer.java
@@ -13,11 +13,23 @@ public class GameServer {
 
 	public static void main(String[] args) {
 		assert System.getProperty("cyc.home") != null;
+		armSecurityManager();
+		startServer();
+	}
 
+	private static void startServer() {
 		try (TcpHandler tcpHandler = new TcpHandler()) {
 			tcpHandler.runServer();
 		} catch (IOException | InterruptedException e) {
 			log.error("Failed to start StartEngine", e);
+		}
+	}
+
+	private static void armSecurityManager() {
+		if ("disable".equalsIgnoreCase(System.getProperty("cyc.security", "enabled"))) {
+			log.error("SecurityManager disbaled!");
+		} else {
+			System.setSecurityManager(new SecurityManager());
 		}
 	}
 
