c7550e3b93457feb7c5d4737aa03d3ba90a3b82c
Oli Zimpasser <oglimmer@gmail.com>
2014-03-08 13:26:18 +0100

    typo

--- vagrant/bootstrap_backend.sh
+++ vagrant/bootstrap_backend.sh
@@ -13,7 +13,7 @@ cp -r /vagrant/backend/* /usr/local/cyc-engine
 cp /vagrant/cyc-engine /etc/init.d
 chmod 755 /etc/init.d/cyc-engine
 
-# create securit.policy for 3-host configuration
+# create security.policy for 3-host configuration
 cp /vagrant/backend/security.policy /tmp/cyc-security.policy
 sed 's/permission java.net.SocketPermission "127.0.0.1:-", "accept,resolve";/permission java.net.SocketPermission "192.168.33.2:-", "accept,resolve";/g' /tmp/cyc-security.policy  > /tmp/cyc-security.policy.1
 sed 's/permission java.net.SocketPermission "localhost", "resolve";/permission java.net.SocketPermission "localhost", "resolve,listen";/g' /tmp/cyc-security.policy.1  > /tmp/cyc-security.policy.2
