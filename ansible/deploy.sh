0964c405949c6b806595e481fdc19603ef40aeb8
Oli Zimpasser <oglimmer@gmail.com>
2014-03-15 20:16:09 +0100

    Refactored cyc-container

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,14 +3,16 @@
 target_environment=$1
 build_yml=$2
 echo "Setting up $build_yml on $target_environment"
+shift
+shift
 
 # create VM(s)
 cd $target_environment
-vagrant up
+#vagrant up
 cd ..
 
 # build and copy artifacts to roles/../files
-./build.sh
+#./build.sh
 
 # prepare insecure ssh
 rm -f /tmp/vagrant.key 
@@ -19,4 +21,4 @@ chmod 400 /tmp/vagrant.key
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 # provision
-ansible-playbook $build_yml.yml --user=vagrant --sudo --timeout=100 --inventory-file=$target_environment/inventory --private-key=/tmp/vagrant.key --module-path modules
+ansible-playbook $build_yml.yml --user=vagrant --sudo --timeout=100 --inventory-file=$target_environment/inventory --private-key=/tmp/vagrant.key --module-path modules $*
