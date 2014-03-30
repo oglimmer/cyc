ccc6c66894bb777855991704c7be0add02b0c00c
Oli Zimpasser <oglimmer@gmail.com>
2014-03-31 00:13:00 +0200

    Fix that tomcat and standalone cyc-engine work in regards of file permissions to logs

--- cyc_mgmt.sh
+++ cyc_mgmt.sh
@@ -91,7 +91,7 @@ if [ -n "$ENGINE_CONTAINER" ]; then
 	cp engine-container/target/engine-container-jar-with-dependencies.jar $CYC_ENGINE_CONTAINER
 	cp engine-container/target/classes/security.policy $CYC_ENGINE_CONTAINER	
 	mkdir -p $CYC_ENGINE_CONTAINER/logs
-	echo "java -Xmx256M -XX:MaxPermSize=256M -Djava.security.policy=security.policy -Dcyc.home=.  -jar engine-container-jar-with-dependencies.jar" >$CYC_ENGINE_CONTAINER/run.sh
+	echo '#!/bin/bash\n\numask 0002\n\njava -Xmx256M -XX:MaxPermSize=256M -Djava.security.policy=security.policy -Dcyc.home=.  -jar engine-container-jar-with-dependencies.jar' >$CYC_ENGINE_CONTAINER/run.sh
 	chmod 777 $CYC_ENGINE_CONTAINER/run.sh
 fi
 
