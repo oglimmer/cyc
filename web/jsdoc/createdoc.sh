ecba69bc0799a700db6169c0348bc1d2e513a68e
Oli Zimpasser <oglimmer@gmail.com>
2016-02-01 23:42:08 +0100

    Installs ink-docstrap npm modules for JSDoc generation

--- web/jsdoc/createdoc.sh
+++ web/jsdoc/createdoc.sh
@@ -2,6 +2,7 @@
 
 jsdoc -v 1>/dev/null
 if  [ $? -eq 0 ]; then
+	npm install ink-docstrap
 	rm -rf ../src/main/webapp/apidocs/* 
 	jsdoc --verbose -d ../src/main/webapp/apidocs/ -c conf.json -t node_modules/ink-docstrap/template README.md .
 fi
