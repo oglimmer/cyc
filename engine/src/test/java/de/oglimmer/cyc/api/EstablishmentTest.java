55ac21bcfb071a20f3398a44fc6992a7788bfaa1
Tim Feuerbach <tim_frederic.feuerbach@stud.tu-darmstadt.de>
2016-05-18 19:50:00 +0200

    removed unused import

--- engine/src/test/java/de/oglimmer/cyc/api/EstablishmentTest.java
+++ engine/src/test/java/de/oglimmer/cyc/api/EstablishmentTest.java
@@ -5,7 +5,6 @@ import de.oglimmer.cyc.api.Constants.Mode;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Iterator;
 
