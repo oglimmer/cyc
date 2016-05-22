e955634e7fc19a8e933d9ac7d48d8c5f295031e3
Oli Zimpasser <oglimmer@gmail.com>
2016-05-22 13:09:51 +0200

    Remove . after password in password forgotten email

--- web/src/main/java/de/oglimmer/cyc/web/util/EmailService.java
+++ web/src/main/java/de/oglimmer/cyc/web/util/EmailService.java
@@ -44,7 +44,7 @@ public enum EmailService {
 
 	public void sendPasswordReset(String email, String newPass) {
 		createAndSendMail(email, "New password for codeyourrestaurant.com",
-				"Hello user,\n\nyour new password is : " + newPass + ".\n\nYou should login now and change it.");
+				"Hello user,\n\nyour new password is: " + newPass + "\n\nYou should login now and change it.");
 	}
 
 	public void informAdminRegister(String username, String email) {
