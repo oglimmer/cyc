90fd864ac87b40dd9fc32519cbbd7ed7dd30a9c3
Oli Zimpasser <oglimmer@gmail.com>
2016-11-17 20:11:54 +0100

    Update DO to Ubuntuu 16.04

--- ansible/digital_ocean/README.md
+++ ansible/digital_ocean/README.md
@@ -1,7 +1,7 @@
 SETUP
 -----
 
-1.) install the python module dopy (e.g. pip install dopy)
+1.) install the python module dopy and six (e.g. pip install dopy six)
 
 2.) set the env variables DO_API_TOKEN for our digital ocean account
 
--- ansible/digital_ocean/settings.sh
+++ ansible/digital_ocean/settings.sh
@@ -6,5 +6,5 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 # create VM(s)
 cd ansible/digital_ocean
-ansible-playbook create_vm.yml --inventory-file=create_vm_inventory
+ansible-playbook create_vm.yml --inventory-file=create_vm_inventory || exit 1
 cd ../..
