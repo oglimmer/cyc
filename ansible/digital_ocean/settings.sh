c4d42b80dcfb152c6c2b9694b926ab3085081661
Oli Zimpasser <oglimmer@gmail.com>
2016-12-08 22:21:45 +0100

    Ansible for digital ocean uploads a ssh key

--- ansible/digital_ocean/settings.sh
+++ ansible/digital_ocean/settings.sh
@@ -4,6 +4,11 @@ SSHUSER=root
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+	echo "Faild to read ~/.ssh/id_rsa.pub"
+	exit 1
+fi
+
 # create VM(s)
 cd ansible/digital_ocean
 ansible-playbook create_vm.yml --inventory-file=create_vm_inventory || exit 1
