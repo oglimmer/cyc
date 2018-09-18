9f2604779a7cee06d57c785007b1b4f51d05f019
Oli Zimpasser <oglimmer@gmail.com>
2018-09-18 23:22:37 +0200

    Add config files to Fulgensfile

--- Fulgensfile.js
+++ Fulgensfile.js
@@ -15,7 +15,7 @@ module.exports = {
       Artifact: "web/target/cyr##001.war"
     },
 
-    "couchdb": {
+    "db": {
       Source: "couchdb",
       CouchDB: {
         Schema: "cyc",
@@ -31,6 +31,7 @@ module.exports = {
       Source: "shell",
       Start: "$$TMP$$/cyc-engine-container/run.sh",
       ExposedPort: 9998,
+      DockerImage: "openjdk",
       EnvVars: [
         "CYC_ENGINE_CONTAINER=$$TMP$$/cyc-engine-container"
       ],
@@ -44,12 +45,27 @@ module.exports = {
         "chmod 777 $CYC_ENG_CON_PATH/run.sh",
         "mkdir -p $CYC_ENG_CON_PATH/cyc001",
         "cp engine/target/engine-jar-with-dependencies.jar $CYC_ENG_CON_PATH/cyc001"
-      ]
+      ],
+      config: {
+        Name: "cyc_engine.properties",
+        Connections: [ { Source:"db", Var: "couchdb.host" } ],
+        Content: [ "bind=0.0.0.0" ],
+        AttachAsEnvVar: ["OPTS", "-Dcyc.properties=$$SELF_NAME$$"]        
+      }
     },
 
-    "tomcat": {
+    "web": {
       Source: "tomcat",
-      Deploy: "cyc_build"
+      Deploy: "cyc_build",
+      config: {
+        Name: "cyc_web.properties",
+        Connections: [ 
+          { Source:"db", Var: "couchdb.host" },
+          { Source:"cyc_engine_container", Var: "engine.host.full" },
+          { Source:"cyc_engine_container", Var: "engine.host.test" }
+        ],
+        AttachAsEnvVar: ["JAVA_OPTS", "-Dcyc.properties=$$SELF_NAME$$"]        
+      }
     }
   }
 }
