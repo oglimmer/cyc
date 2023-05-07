bc54b522423f355a72b31025523526c802fa1fec
Oli Zimpasser <oglimmer@gmail.com>
2023-05-07 19:28:01 +0200

    Update createdoc.sh

--- web/jsdoc/createdoc.sh
+++ web/jsdoc/createdoc.sh
@@ -2,7 +2,7 @@
 
 jsdoc --help 1>/dev/null
 if  [ $? -eq 0 ]; then
-	npm install ink-docstrap
+	npm install ink-docstrap taffydb
 	rm -rf ../src/main/webapp/apidocs/* 
 	jsdoc --verbose -d ../src/main/webapp/apidocs/ -c conf.json -t node_modules/ink-docstrap/template README.md .
 fi
